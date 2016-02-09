41207ee6d87462453c44244694ff3a754fc3bd41
Ethan Lampert <ethanlampert@wl-10-190-50-214.wireless.duke.edu>
2016-02-09 17:53:39 -0500

    Ethan Commit

--- ToDoList/ToDoList/AppDelegate.swift
+++ ToDoList/ToDoList/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  ToDoList
 //
-//  Created by Davis Gossage on 2/1/16.
-//  Copyright © 2016 Davis Gossage. All rights reserved.
+//  Created by Ethan Lampert on 2/3/16.
+//  Copyright © 2016 Ethan Lampert. All rights reserved.
 //
 
 import UIKit
